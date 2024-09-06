f114055ef92df99d477fc4e9cb37d21472c19531
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2024-09-06 11:24:28 -0300

    Versão 1.1. Adicionado requirements.txt e tratamento de exceção no app.py.

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from rembg import remove
 from PIL import Image
 
 while True:
-    imagem = input('Informe o nome da imagem com a extensão: ')
-    if imagem != '':
-        nova_imagem = f'nova_{imagem}.png'
-        entrada = Image.open(imagem)
-        saida = remove(entrada)
-        saida.save(nova_imagem)
-        continue
-    else:
+    try:
+        imagem = input('Informe o nome da imagem com a extensão ou Enter para encerrar: ')
+        if imagem != '':
+            nova_imagem = f'nova_{imagem}.png'
+            entrada = Image.open(imagem)
+            saida = remove(entrada)
+            saida.save(nova_imagem)
+            continue
+        else:
+            ...
+    except Exception as e:
+        print(f'Não foi possível remover o fundo da imagem. {e}.')
+    finally:
         break
